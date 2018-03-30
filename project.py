940ad1b55b7c9fa083ad73ba994b184bd96bc843
Botmasher <languagejosh@hotmail.com>
2018-03-30 09:45:37 -1000

    Draft methods for calculating pangrammatic ratios

--- project.py
+++ project.py
@@ -9,6 +9,9 @@ class Pangrammatron:
 			'phones': 'dictionaries/en/cmudict-0.7b.phones'
 		}
 	}
+	memo_phones = {}
+	memo_letters = {}
+
 	def __init__ (self, alphabet, language='en'):
 		# TODO check and clean alphabet before storing
 			# if alphabet is string of characters, split into array
@@ -42,19 +45,14 @@ class Pangrammatron:
 		self.alphabet = alphabet
 		return self.alphabet
 
-	def is_pangram (self, text):
-		# TODO when asked if a sentence is a pangram
-			# uncaps sentence - actually check if upper or lower match alphabet since alphabet can have either
-			# use regex to break sentence into words
-			# answer True if every letter in self.alphabet is in the sentence
-			# answer False otherwise
-	
-		# TODO split on self.alphabet's characters instead of fixed a-z
-		words = re.compile(r'[^%s]+' % "".join(self.alphabet)).split(text.upper())
-		
-		letters = "".join(words)
+	def break_into_words (self, text):
+		regex = r'[^%s]+' % "".join(self.alphabet)
+		words = re.compile(regex).split(text.upper())
+		return words
 
-		# TODO if answer is memoized and is 100%, return True
+	def unique_letters (self, words):
+		# TODO split on self.alphabet's characters instead of fixed a-z
+		letters = "".join(words)
 
 		# use hash set to check for uniques
 		found_unique_letters = set([])
@@ -62,27 +60,12 @@ class Pangrammatron:
 			if letter.upper() in self.alphabet or letter.lower() in self.alphabet:
 				found_unique_letters.add(letter)
 
-		# TODO memoize answer
+		return found_unique_letters
 
-		# whether every letter was found in text
-		if len(found_unique_letters) >= len(self.alphabet): return True
-		return False
-
-	def is_panphone (self, text):
-		# TODO when asked if a sentence is a panphone
-			# caps sentence (for CMU dict)
-			# use regex to break sentence into words
-			# break words into phonemes
-			# iterate through phonemes
-			# answer True if every phone in self.phones_dict is in the sentence
-			# answer False otherwise
-		# TODO if answer is memoized and is 100%, return True
-
-		words = re.compile(r'[^%s]+' % "".join(self.alphabet)).split(text.upper())
+	def unique_phones (self, words):
 		found_unique_phones = set([])
 		words = set(words)
 		if '' in words: words.remove('')
-		print (words)
 
 		with open(self.phones_lexicon, "r") as f:
 			for l in f:
@@ -97,11 +80,8 @@ class Pangrammatron:
 					for phone in l_phones:
 						phoneme = "".join([c for c in phone if not c.isdigit()])
 						found_unique_phones.add(phoneme)
-		print (found_unique_phones)
-				
-		# whether every letter was found in text
-		if len(found_unique_phones) >= len(self.phones_inventory): return True
-		return False
+
+		return found_unique_phones
 
 	def how_pangrammatic (self, sentence):
 		# TODO when asked how much of a pangram a sentence is
@@ -110,7 +90,20 @@ class Pangrammatron:
 		# TODO consider doing main work here and calling from is_pangram
 			# is_pangram will return True if ratio is 100%
 			# memoize answer per sentence
-		return None
+
+		words = self.break_into_words (sentence)
+
+		# memoized answer
+		if "".join(words) in self.memo_letters:
+			uniques = self.memo_letters["".join(words)]
+		# calculated answer
+		else:
+			uniques = self.unique_letters (words)
+			self.memo_letters["".join(words)] = uniques
+
+		ratio = {'sample': len(uniques), 'total': len(self.alphabet)}
+
+		return (ratio['sample'] / ratio['total'])
 
 	def how_panphonic (self, sentence):
 		# TODO when asked how much of a panphone a sentence is
@@ -119,10 +112,57 @@ class Pangrammatron:
 		# TODO consider doing main work here and calling from is_panphone
 			# is_panphone will return True if ratio is 100%
 			# memoize answer per sentence
-		return None
+
+		words = self.break_into_words (sentence)
+
+		# memoized answer
+		if "".join(words) in self.memo_phones:
+			uniques = self.memo_phones["".join(words)]
+		# calculated answer
+		else:
+			uniques = self.unique_phones (words)
+			self.memo_phones["".join(words)] = uniques
+
+		ratio = {'sample': len(uniques), 'total': len(self.phones_inventory)}
+
+		return (ratio['sample'] / ratio['total'])
+
+	def is_pangram (self, text):
+		# TODO when asked if a sentence is a pangram
+			# uncaps sentence - actually check if upper or lower match alphabet since alphabet can have either
+			# use regex to break sentence into words
+			# answer True if every letter in self.alphabet is in the sentence
+			# answer False otherwise
+
+		# TODO memoize answer
+
+		# whether every letter was found in text
+		ratio = self.how_pangrammatic(text)
+
+		if ratio >= 1.0: return True
+		return False
+
+	def is_panphone (self, text):
+		# TODO when asked if a sentence is a panphone
+			# caps sentence (for CMU dict)
+			# use regex to break sentence into words
+			# break words into phonemes
+			# iterate through phonemes
+			# answer True if every phone in self.phones_dict is in the sentence
+			# answer False otherwise
+		# TODO if answer is memoized and is 100%, return True
+
+		ratio = self.how_panphonic(text)
+				
+		# whether every letter was found in text
+		if ratio >= 1.0: return True
+		return False
 
 if __name__ == '__main__':
 	pan = Pangrammatron("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 	print(pan.is_pangram("The quick brown fox jumped over the lazy dog."))
+	print(pan.how_pangrammatic("The quick brown fox jumped over the lazy dog."))
 	print(pan.is_pangram("The quick brown fox jumped over the lazy dogs."))
+	print(pan.how_pangrammatic("The quick brown fox jumped over the lazy dogs."))
 	print(pan.is_panphone("The quick brown fox jumped over the lazy dog."))
+	print(pan.how_panphonic("The quick brown fox jumped over the lazy dog."))
