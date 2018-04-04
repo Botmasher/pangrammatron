80bc00d6d958badaa9189c91f16bc0bb546d87de
Botmasher <languagejosh@hotmail.com>
2018-04-04 13:38:28 -1000

    Parse and count panphones in JS

--- js/pangrammatron.js
+++ js/pangrammatron.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 
 // TODO break out text parser to access phones data through dedicated JS object
+
 const loadPhones = (uri) => {
 	const h = new XMLHttpRequest();
 	h.open('GET', uri);
@@ -15,6 +16,8 @@ const loadPhones = (uri) => {
 	};
 	h.send(null);
 }
+
+// TODO include memo
 
 class Pangrammatron {
 	constructor(alphabet=[], inventory=[], language='en') {
@@ -93,7 +96,39 @@ class Pangrammatron {
 	}
 
 	howPanphonic(text) {
-		return false;
+		if (!this.inventory || this.inventory.size < 1) return;
+		
+		const words = text.toUpperCase().match(/[A-Z]+/g);
+
+		return new Promise((resolve, reject) => {
+			fs.readFile(`../${this.files.en.lex}`,
+				(error, data) => {
+					const phones = new Set();
+					for (let line of data.toString('utf-8').match(/[^\n]+/g)) {
+						const l_elems = line.match(/[^ \t]+/g);
+						if (l_elems && l_elems.length > 1 && words.includes(l_elems[0])) {
+							l_elems.slice(1, l_elems.length).map(phone => phones.add(phone));
+						}
+					}
+					resolve(phones);
+				}
+			);
+		});
+
+		// with open(self.phones_lexicon, "r") as f:
+		// 	for l in f:
+		// 		try:
+		// 			l_elems = l.split()
+		// 			l_word = l_elems[0]
+		// 			l_phones = l_elems[1:]
+		// 		except:
+		// 			# line is not a formatted lexicon entry
+		// 			continue
+		// 		if l_word in words:
+		// 			# strip numbers from end of some phones
+		// 			for phone in l_phones:
+		// 				phoneme = "".join([c for c in phone if not c.isdigit()])
+		// 				found_unique_phones.add(phoneme)
 	}
 	
 	isPangram(text) {
@@ -102,7 +137,9 @@ class Pangrammatron {
 	}
 
 	isPanphone(text) {
-		return false;
+		if (!this.inventory || this.inventory.size < 1) return;
+		const phonesPromise = this.howPanphonic(text);
+		return phonesPromise.then(phoneCount => (phoneCount >= this.inventory.size));
 	}
 }
 
@@ -113,6 +150,8 @@ pan.setAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 console.log(pan.isPangram(sent0));
 console.log(pan.isPangram(sent1));
 pan.gatherPhones().then(() => {
-	console.log(pan.isPanphone(sent0));
-	console.log(pan.isPanphone(sent1));
+	pan.isPanphone(sent0).then(count => console.log(count));
+	pan.isPanphone(sent1).then(count => console.log(count)).then(()=> {
+		pan.howPanphonic(sent1).then(phones => console.log(phones.size/pan.inventory.size));
+	});
 });
