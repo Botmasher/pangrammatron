b6be2c42181f25b55f61bc1268347d1c0be905ff
Botmasher <languagejosh@hotmail.com>
2018-04-05 12:02:10 -1000

    Add regex for handling contractions and memo for previous solutions to JS

--- js/pangrammatron.js
+++ js/pangrammatron.js
@@ -17,7 +17,7 @@ const loadPhones = (uri) => {
 	h.send(null);
 }
 
-// TODO include memo
+// TODO redo path (the relative one breaks when run from any other dir e.g. node js/p~.js from ../)
 
 class Pangrammatron {
 	constructor(alphabet=[], inventory=[], language='en') {
@@ -30,6 +30,10 @@ class Pangrammatron {
 		this.language = this.setLanguage(language);
 		this.alphabet = this.setAlphabet(alphabet);
 		this.inventory = this.setInventory(inventory);
+		this.memo = {
+			phones: {},
+			grams: {}
+		};
 	}
 	
 	gatherPhones(cb) {
@@ -86,7 +90,7 @@ class Pangrammatron {
 	}
 	
 	breakIntoWords(text) {
-		const words = text.toUpperCase().match(/[A-Z]+/g); 	// TODO handle contractions
+		const words = text.toUpperCase().match(/([A-Z](\'[A-Z]+)?)+/g);
 		const splitText = text.trim().split(" ");
 		return new Set(words);
 	}
@@ -99,7 +103,7 @@ class Pangrammatron {
 		return foundLetters;
 	}
 
-	// TODO revisit once phones inventory and lexicon parser-builder is in place
+	// TODO revisit after phones inventory and lexicon parser-builder is in place
 	uniquePhones(words) {
 		const phones = new Set();
 		// for (let word of words) {
@@ -112,27 +116,47 @@ class Pangrammatron {
 	howPangrammatic(text) {
 		if (!this.alphabet) throw "No alphabet defined before calling Pangrammatron.howPangrammatic"
 
+		const words = text.toUpperCase().match(/([A-Z](\'[A-Z]+)?)+/g); 	
+		console.log(words);
+		const cleanedText = words.join();
+
+		if (this.memo.grams.cleanedText) return this.memo.grams.cleanedText.size;
+
 		const foundLetters = new Set();
 		for (let letter of text.toUpperCase()) {
 			this.alphabet.has(letter) && foundLetters.add(letter);
 		}
+		this.memo.grams[cleanedText] = foundLetters;
 		return foundLetters.size; 	// TODO separate method for returning alphabet size
 	}
 
 	howPanphonic(text) {
 		if (!this.inventory) throw "No inventory defined before calling Pangrammatron.howPanphonic";
 		
-		const words = text.toUpperCase().match(/[A-Z]+/g);
+		// split and scrub text
+		const words = text.toUpperCase().match(/([A-Z](\'[A-Z]+)?)+/g); 	
+		console.log(words);
+		const cleanedText = words.join();
+
 		return new Promise((resolve, reject) => {
+			// check for previous solution
+			if (this.memo.phones.cleanedText) resolve(this.memo.phones.cleanedText);
+
 			fs.readFile(`../${this.files.en.lex}`,
 				(error, data) => {
 					const phones = new Set();
+					
+					// search lines for words from text and get phones from matches
 					for (let line of data.toString('utf-8').match(/[^\n]+/g)) {
 						const l_elems = line.match(/[^ \t]+/g);
 						if (l_elems && l_elems.length > 1 && words.includes(l_elems[0])) {
 							l_elems.slice(1, l_elems.length).map(phone => phones.add(phone));
 						}
 					}
+
+					// save solution
+					this.memo.phones[cleanedText] = phones;
+
 					resolve(phones);
 				}
 			);
@@ -153,7 +177,7 @@ class Pangrammatron {
 
 pan = new Pangrammatron();
 const sent0 = "The quick brown fox jumped over the lazy dog.";
-const sent1 = "The quick brown fox jumped over the lazy dogs.";
+const sent1 = "The quick brown fox jumped over the lazy dog's.";
 pan.setAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 console.log(pan.isPangram(sent0));
 console.log(pan.isPangram(sent1));
