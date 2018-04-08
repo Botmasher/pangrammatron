532eb5c6a5d39f9f1510c2c9c99d35b76955f718
Botmasher <languagejosh@hotmail.com>
2018-04-07 17:49:03 -1000

    Separate js lex and phones file loading from panphone check

--- js/pangrammatron.js
+++ js/pangrammatron.js
@@ -4,51 +4,69 @@ const fs = require('fs');
 
 // TODO break out text parser to access phones data through dedicated JS object
 
-const loadPhones = (uri) => {
-	const h = new XMLHttpRequest();
-	h.open('GET', uri);
-	h.onreadystatechange = () => {
-		const txt = h.readyState === 4 && h.status === 200
-			? h.responseText
-			: null
-		;
-		return txt;
-	};
-	h.send(null);
-}
-
-// TODO redo path (the relative one breaks when run from any other dir e.g. node js/p~.js from ../)
-
-class Pangrammatron {
-	constructor(alphabet=[], inventory=[], language='en') {
-		this.files = {
+class CMUPhonesDictionary {
+	constructor() {
+		this.paths = {
 			en: {
 				lex: 'dictionaries/en/cmudict-0.7b.txt',
 				phon: 'dictionaries/en/cmudict-0.7b.phones'
 			}
 		};
+		this.phones = {};
+		this.entries = {};
+	}
+
+	gatherPhones(cb) {
+		return new Promise((resolve, reject) => {
+			fs.readFile(`../${this.paths.en.phon}`,
+				(error, data) => {
+					const phones = new Set();
+					for (let line of data.toString('utf-8').match(/[^\n]+/g)) {
+						phones.add(line.split('\t')[0]);
+					}
+					this.phones = phones;
+					resolve(this.phones);
+				}
+			);
+		});
+	}
+
+	gatherEntries() {
+		return new Promise((resolve, reject) => {
+			fs.readFile(`../${this.paths.en.lex}`,
+				(error, data) => {
+					const phones = new Set();
+					let word = '';
+					let sounds = [];
+					// search lines for formatted entries
+					for (let line of data.toString('utf-8').match(/[^\n]+/g)) {
+						const l_elems = line.match(/[^ \t]+/g);
+						// find and store real entries as 'word': [phone_0, ..., phone_n]
+						if (l_elems && l_elems.length > 1 && this.phones.has(l_elems[1])) {
+							word = l_elems[0];
+							sounds = l_elems.slice(1, l_elems.length);
+							this.entries[word] = sounds;
+						}
+					}
+					resolve(this.entries);
+				}
+			);
+		});
+	}
+}
+
+// TODO redo path (the relative one breaks when run from any other dir e.g. node js/p~.js from ../)
+
+class Pangrammatron {
+	constructor(alphabet=[], inventory=[], dictionary={}, language='en') {
 		this.language = this.setLanguage(language);
 		this.alphabet = this.setAlphabet(alphabet);
-		this.inventory = this.setInventory(inventory);
+		this.inventory = inventory;
+		this.dictionary = dictionary;
 		this.memo = {
 			phones: {},
 			grams: {}
 		};
-	}
-	
-	gatherPhones(cb) {
-		return new Promise((resolve, reject) => {
-			fs.readFile(`../${this.files.en.phon}`,
-				(error, data) => {
-					const inventory = new Set();
-					for (let line of data.toString('utf-8').match(/[^\n]+/g)) {
-						inventory.add(line.split('\t')[0]);
-					}
-					this.inventory = inventory;
-					resolve(this.inventory);
-				}
-			);
-		});
 	}
 	
 	setAlphabet(alphabet) {
@@ -68,6 +86,10 @@ class Pangrammatron {
 		return this.inventory;
 	}
 
+	getDictionary() {
+		return this.dictionary;
+	}
+
 	countAlphabet() {
 		return this.alphabet.size;
 	}
@@ -84,6 +106,10 @@ class Pangrammatron {
 		return this.inventory;
 	}
 	
+	setDictionary(dictionary) {
+		this.dictionary = dictionary;
+	}
+
 	setLanguage(language) {
 		if (language.length !== 2) return false;
 		this.language = language;
@@ -138,29 +164,21 @@ class Pangrammatron {
 		console.log(words);
 		const cleanedText = words.join();
 
-		return new Promise((resolve, reject) => {
-			// check for previous solution
-			if (this.memo.phones.cleanedText) resolve(this.memo.phones.cleanedText);
+		if (this.memo.phones.cleanedText) return (this.memo.phones.cleanedText);
 
-			fs.readFile(`../${this.files.en.lex}`,
-				(error, data) => {
-					const phones = new Set();
-					
-					// search lines for words from text and get phones from matches
-					for (let line of data.toString('utf-8').match(/[^\n]+/g)) {
-						const l_elems = line.match(/[^ \t]+/g);
-						if (l_elems && l_elems.length > 1 && words.includes(l_elems[0])) {
-							l_elems.slice(1, l_elems.length).map(phone => phones.add(phone));
-						}
-					}
+		const phones = new Set();
 
-					// save solution
-					this.memo.phones[cleanedText] = phones;
+		for (let word of words) {
+			if (!this.dictionary[word]) continue;
+			for (let sound of this.dictionary[word]) {
+				phones.add(sound);
+			}
+		}
 
-					resolve(phones);
-				}
-			);
-		});
+		// save solution
+		this.memo.phones[cleanedText] = phones;
+
+		return (phones);
 	}
 	
 	isPangram(text) {
@@ -179,11 +197,10 @@ pan = new Pangrammatron();
 const sent0 = "The quick brown fox jumped over the lazy dog.";
 const sent1 = "The quick brown fox jumped over the lazy dog's.";
 pan.setAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-console.log(pan.isPangram(sent0));
-console.log(pan.isPangram(sent1));
-pan.gatherPhones().then(() => {
-	pan.isPanphone(sent0).then(count => console.log(count));
-	pan.isPanphone(sent1).then(count => console.log(count)).then(()=> {
-		pan.howPanphonic(sent1).then(phones => console.log(phones.size/pan.inventory.size));
-	});
-});
+cmu = new CMUPhonesDictionary();
+cmu.gatherPhones().then((inventory) => cmu.gatherEntries().then((entries) => {
+	pan.setInventory(inventory);
+	pan.setDictionary(entries);
+	console.log(pan.isPanphone(sent0));
+	console.log(pan.isPanphone(sent1));
+}));
